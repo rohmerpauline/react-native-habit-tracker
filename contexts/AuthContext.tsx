3ff395316d1eef4c0d09c80eca53e176fa1c090f
rohmerpauline <rohmer.pauline@outlook.com>
2025-10-28 15:59:32 +0100

    feat: implement authentication flow

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,34 +1,96 @@
-import { createContext, use, useState, type PropsWithChildren } from 'react';
+import { account } from '@/lib/appwrite';
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type PropsWithChildren,
+} from 'react';
+import { ID, Models } from 'react-native-appwrite';
 
-const AuthContext = createContext<{
-  session: boolean;
-  isLoading: boolean;
-}>({
-  session: false,
-  isLoading: false,
-});
+type AuthContextType = {
+  user: Models.User<Models.Preferences> | null;
+  isLoadingUser: boolean;
+  signUp: (email: string, password: string) => Promise<string | null>;
+  signIn: (email: string, password: string) => Promise<string | null>;
+  signOut: () => Promise<void>;
+};
 
-export function SessionProvider({ children }: PropsWithChildren) {
-  const [session, setSession] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: PropsWithChildren) {
+  const [user, setUser] = useState<Models.User<Models.Preferences> | null>(
+    null,
+  );
+  const [isLoadingUser, setIsLoadingUser] = useState<boolean>(true);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const signUp = async (email: string, password: string) => {
+    try {
+      await account.create(ID.unique(), email, password);
+      await signIn(email, password);
+      await getUser();
+      return null;
+    } catch (error) {
+      if (error instanceof Error) {
+        return error.message;
+      }
+
+      return 'An error occured during sign in.';
+    }
+  };
+
+  const signIn = async (email: string, password: string) => {
+    try {
+      await account.createEmailPasswordSession(email, password);
+      await getUser();
+      return null;
+    } catch (error) {
+      if (error instanceof Error) {
+        return error.message;
+      }
+
+      return 'An error occured during signup.';
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await account.deleteSession('current');
+      setUser(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getUser = async () => {
+    try {
+      const session = await account.get();
+      setUser(session);
+    } catch (error) {
+      setUser(null);
+    } finally {
+      setIsLoadingUser(false);
+    }
+  };
 
   return (
-    <AuthContext
-      value={{
-        session,
-        isLoading,
-      }}
+    <AuthContext.Provider
+      value={{ user, isLoadingUser, signUp, signIn, signOut }}
     >
       {children}
-    </AuthContext>
+    </AuthContext.Provider>
   );
 }
 
 // Use this hook to access the user info.
-export function useSession() {
-  const value = use(AuthContext);
+export function useAuth() {
+  const value = useContext(AuthContext);
   if (!value) {
-    throw new Error('useSession must be wrapped in a <SessionProvider />');
+    throw new Error('useAuth must be wrapped in a <AuthProvider />');
   }
 
   return value;
